dda562a0cff8bd4bbc86f54f6fe402579c5e51ea
seongkah <seongkah@gmail.com>
2020-05-26 02:59:41 +0800

    Add files via upload

---
+++ result.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPharse {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You Good';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are strange';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        resultPharse,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
